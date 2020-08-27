82b44893dc9c2e5512991919568290876cd2dcee
Tetris4731 <68471468+Tetris4731@users.noreply.github.com>
2020-08-27 15:30:56 +0300

    Add files via upload

---
+++ MyArrayDataException.java
@@ -0,0 +1,19 @@
+package src.home_work_2p;
+
+public class MyArrayDataException extends RuntimeException{
+    private int row;
+    private int col;
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+    public MyArrayDataException(String message, int row, int col) {
+        super(message);
+        this.row = row;
+        this.col = col;
+    }
+}
